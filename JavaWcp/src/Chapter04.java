6e23332f1dcea194465671d131c39f664181b0cb
tom-ok-i <ten21.t.tom@gmail.com>
2021-12-22 05:58:56 +0900

    Java basic lesson 05 done

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Tomoki Takata";
+		System.out.println(name);
+		
+		name = "hoge";
+		System.out.println(name);
+		
+		final int luckynum = 777;
+		System.out.println(luckynum);
+	}
+
+}
---
+++ JavaWcp/src/Variable02.java
@@ -0,0 +1,14 @@
+
+public class Variable02 {
+	
+	public static void main(String[] args) {
+		
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+		
+		//MIN_NUMBER = 20;
+		//定数につき再代入不可
+		
+	}
+
+}
