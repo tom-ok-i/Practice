5c695f4b97096b510d9e70dd17fdc5da6a5ac646
tom-ok-i <ten21.t.tom@gmail.com>
2021-12-23 03:11:18 +0900

    Java basic lesson 06 begin

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 12345678910L;
+		double double1 = 3.1415926535;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float01 = 3.14f;
+		Integer int1 = 123;
+		Character c = 'あ';
+		System.out.println(float01);
+		System.out.println(int1);
+		System.out.println(c);
+		
+		int[] array1 = new int[] {10, 20, 30, 40};
+		System.out.println(array1[3]);
+	}
+}
