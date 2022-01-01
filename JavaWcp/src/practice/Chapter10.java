e4059384e1cf76592efeb2b6f5e6b76673e4de4e
tom-ok-i <ten21.t.tom@gmail.com>
2022-01-01 19:40:58 +0900

    Java basic lesson chapter 10 during final excercise

---
+++ JavaWcp/src/practice/Chapter10.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.sql.SQLException;
+
+public class Chapter10 {
+	public static void main(String[] args) {
+		
+		try {
+			int array [] = { 1, 3, 5 };
+			Chapter10.validIndex(array, 2);
+			Chapter10.validIndex(array, 3);
+			
+			System.out.println("validIndexの呼び出し終了");
+		} catch (IllegalArgumentException e) {
+			System.out.println("IllegalArgumentExceptionが発生しました");
+			e.printStackTrace();
+		}
+		System.out.println("mainメソッド終了");
+	}
+	
+	public static void throwSQLException() throws SQLException {
+		throw new SQLException("SQLエラーです");
+	}
+}
